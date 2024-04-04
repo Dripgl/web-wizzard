331c35b8937bb298e1a1dd9d184bb1296e8ffcda
Gabri <gabriele.zito@lc-sw.com>
2024-04-04 22:16:05 +0200

    timeline upgrade

--- src/page/Processing/Timeline.jsx
+++ src/page/Processing/Timeline.jsx
@@ -1,86 +1,101 @@
 import React from "react";
-import "./Timeline.css"
-import 'react-vertical-timeline-component/style.min.css';
+import "./Timeline.css";
+import "react-vertical-timeline-component/style.min.css";
 import { motion } from "framer-motion";
 
-
 function ProcessLeft() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="main-timeline">
-                        <motion.div
-                            className="timeline"
-                            whileInView={{ scale: 1.2 }} // Animazione quando l'elemento è nella vista
-                        >
-                            <div href="#" className="timeline-content">
-                                <div className="timeline-icon">
-                                    {/* <i className="fa fa-globe"></i> */}
-                                </div>
-                                <h3 className="title">Web Designing</h3>
-                                <p className="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                </p>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                            className="timeline"
-                            whileInView={{ scale: 1.2 }} // Animazione quando l'elemento è nella vista
-                        >
-                            <div href="#" className="timeline-content">
-                                <div className="timeline-icon">
-                                    {/* <i className="fa fa-globe"></i> */}
-                                </div>
-                                <h3 className="title">Web Designing</h3>
-                                <p className="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                </p>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                            className="timeline"
-                            whileInView={{ scale: 1.2 }} // Animazione quando l'elemento è nella vista
-                        >
-                            <div href="#" className="timeline-content">
-                                <div className="timeline-icon">
-                                    {/* <i className="fa fa-globe"></i> */}
-                                </div>
-                                <h3 className="title">Web Designing</h3>
-                                <p className="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                </p>
-                            </div>
-                        </motion.div>
-                        <motion.div
-                            className="timeline"
-                            whileInView={{ scale: 1.2 }} // Animazione quando l'elemento è nella vista
-                        >
-                            <div href="#" className="timeline-content">
-                                <div className="timeline-icon">
-                                    {/* <i className="fa fa-globe"></i> */}
-                                </div>
-                                <h3 className="title">Web Designing</h3>
-                                <p className="description">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                    Integer males uada tellus lorem, et condimentum neque commodo
-                                </p>
-                            </div>
-                        </motion.div>
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
+  const variant = {
+    visible: { scale: 1 },
+    hidden: { scale: 0 },
+  };
 
-    )
+  return (
+    <div className="container_timeline">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="main-timeline">
+            <motion.div
+              className="timeline"
+              whileHover={{ scale: 1.1 }} // Animazione quando il mouse è sopra la timeline
+              variants={variant}
+              initial="hidden"
+              whileInView="visible"
+            >
+              <div className="timeline-content">
+                <div className="timeline-icon">
+                  {/* <i className="fa fa-globe"></i> */}
+                </div>
+                <h3 className="title">Web Designing</h3>
+                <p className="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              className="timeline"
+              whileHover={{ scale: 1.1 }} // Animazione quando il mouse è sopra la timeline
+              variants={variant}
+              initial="hidden"
+              whileInView="visible"
+            >
+              <div className="timeline-content">
+                <div className="timeline-icon">
+                  {/* <i className="fa fa-globe"></i> */}
+                </div>
+                <h3 className="title">Web Designing</h3>
+                <p className="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              className="timeline"
+              whileHover={{ scale: 1.1 }} // Animazione quando il mouse è sopra la timeline
+              variants={variant}
+              initial="hidden"
+              whileInView="visible"
+            >
+              <div className="timeline-content">
+                <div className="timeline-icon">
+                  {/* <i className="fa fa-globe"></i> */}
+                </div>
+                <h3 className="title">Web Designing</h3>
+                <p className="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                </p>
+              </div>
+            </motion.div>
+            <motion.div
+              className="timeline"
+              whileHover={{ scale: 1.1 }} // Animazione quando il mouse è sopra la timeline
+              variants={variant}
+              initial="hidden"
+              whileInView="visible"
+            >
+              <div className="timeline-content">
+                <div className="timeline-icon">
+                  {/* <i className="fa fa-globe"></i> */}
+                </div>
+                <h3 className="title">Web Designing</h3>
+                <p className="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                  Integer malesuada tellus lorem, et condimentum neque commodo
+                </p>
+              </div>
+            </motion.div>
+            {/* Repetere lo stesso modello per gli altri elementi */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProcessLeft;
